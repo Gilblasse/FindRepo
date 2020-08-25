810e9a6f6cbf916ee90cb4e32992c8015655f2f4
Nethelbert Blasse <12989615+Gilblasse@users.noreply.github.com>
2020-08-25 12:20:32 -0400

    constructed core files and created initial scraper

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const dotenv = require('dotenv');
 
+// enable enviorment variables
+dotenv.config()
 
 // set up express app
 const app = express();
 
 app.use(bodyParser.json())
 // initialize routes
-app.use('/api', require('./routes/api'));
+app.use('/api/v1', require('./routes/api'));
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,60 @@
 const express = require ('express');
 const router = express.Router();
+const puppeteer = require('puppeteer');
+// const metadata = require('github-metadata');
+// const axios = require('axios');
 
-// // get a list of ninjas from the db
-// router.get('/ninjas', function(req, res){
-//     res.send({type: 'GET'});
-// });
+const openPackagePage = async(browser, page)=> {
+    console.log({browser}, {page})
+    await page.goto(`${baseUrl}/blob/master/package.json`, { waitUntil: 'networkidle2' } );
+    
+    const result = await page.evaluate(()=>{
+        const dependencies = document.querySelector('table').innerText.match(/(?<="dependencies": {)(.*?)(?=},)/gs)
+        let newString = ""
+        
+        dependencies[0].split(/[\n "]/).map(txt => txt.trim()).filter(txt => txt !== "").forEach(t => newString += t)
+        
+        const dependenciesList = newString.split(',')
+        return dependenciesList
+    })
+}
 
-// add a new ninja to the db
-router.post('/signUp', function(req, res){
-    // console.log(req.body)
-    res.send({type: 'POST'});
-    // res.end(JSON.stringify({name})
+
+router.post('/repo-packages-info', async (req, res) => {
+
+    const owner = req.body.owner
+    const repo = req.body.repo
+    
+    const baseUrl = `https://github.com/${owner}/${repo}`
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(`${baseUrl}/blob/master/package.json`, { waitUntil: 'networkidle2' } );
+
+    const packages = await page.evaluate(async () => {
+    
+        const dependencies = document.querySelector('table').innerText.match(/(?<="dependencies": {)(.*?)(?=},)/gs)
+        let newString = ""
+        
+        dependencies[0].split(/[\n "]/).map(txt => txt.trim()).filter(txt => txt !== "").forEach(t => newString += t)
+        
+        const dependenciesList = newString.split(',')
+        return dependenciesList
+        // const links = Object.values(document.querySelector('.js-active-navigation-container').querySelectorAll('a'))
+        // const type = links .find(link => link.innerText == "package.json")
+        
+        // result =  type ? await openPackagePage(browser, page) : null
+
+        // return result
+    })
+
+
+    // console.log({packages});
+    await browser.close();
+
+    res.send({packages})
 });
 
-// // update a ninja in the db
-// router.put('/ninjas/:id', function(req, res){
-//     res.send({type: 'PUT'});
-// });
-
-// // delete a ninja from the db
-// router.delete('/ninjas/:id', function(req, res){
-//     res.send({type: 'DELETE'});
-// });
 
 module.exports = router;
+    // res.end(JSON.stringify({name})
