c85314e142c3451bd95b84ad3bfe43a89013bcfe
Nethelbert Blasse <12989615+Gilblasse@users.noreply.github.com>
2020-08-24 23:33:44 -0400

    created first initial setup

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,26 @@
 const express = require ('express');
 const router = express.Router();
 
-// get a list of ninjas from the db
-router.get('/ninjas', function(req, res){
-    res.send({type: 'GET'});
-});
+// // get a list of ninjas from the db
+// router.get('/ninjas', function(req, res){
+//     res.send({type: 'GET'});
+// });
 
 // add a new ninja to the db
-router.post('/ninjas', function(req, res){
+router.post('/signUp', function(req, res){
+    // console.log(req.body)
     res.send({type: 'POST'});
+    // res.end(JSON.stringify({name})
 });
 
-// update a ninja in the db
-router.put('/ninjas/:id', function(req, res){
-    res.send({type: 'PUT'});
-});
+// // update a ninja in the db
+// router.put('/ninjas/:id', function(req, res){
+//     res.send({type: 'PUT'});
+// });
 
-// delete a ninja from the db
-router.delete('/ninjas/:id', function(req, res){
-    res.send({type: 'DELETE'});
-});
+// // delete a ninja from the db
+// router.delete('/ninjas/:id', function(req, res){
+//     res.send({type: 'DELETE'});
+// });
 
 module.exports = router;
