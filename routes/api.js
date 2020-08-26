b0cf71f29405b7286edd32c60f9bd243a2326148
Nethelbert Blasse <12989615+Gilblasse@users.noreply.github.com>
2020-08-25 20:20:43 -0400

    removed some bloated code and added latest verison pkg

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const express = require ('express');
 const router = express.Router();
 const puppeteer = require('puppeteer');
+const latestVersion = require('latest-version');
+var NpmApi = require('npm-api');
+
+
 // const metadata = require('github-metadata');
 // const axios = require('axios');
 
@@ -24,7 +28,7 @@ router.post('/repo-packages-info', async (req, res) => {
 
     const owner = req.body.owner
     const repo = req.body.repo
-    
+
     const baseUrl = `https://github.com/${owner}/${repo}`
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -32,25 +36,14 @@ router.post('/repo-packages-info', async (req, res) => {
     await page.goto(`${baseUrl}/blob/master/package.json`, { waitUntil: 'networkidle2' } );
 
     const packages = await page.evaluate(async () => {
+        const repoPackageTable = document.querySelector('table').innerText
+        const repoPackage = JSON.parse(repoPackageTable)
     
-        const dependencies = document.querySelector('table').innerText.match(/(?<="dependencies": {)(.*?)(?=},)/gs)
-        let newString = ""
-        
-        dependencies[0].split(/[\n "]/).map(txt => txt.trim()).filter(txt => txt !== "").forEach(t => newString += t)
-        
-        const dependenciesList = newString.split(',')
-        return dependenciesList
-        // const links = Object.values(document.querySelector('.js-active-navigation-container').querySelectorAll('a'))
-        // const type = links .find(link => link.innerText == "package.json")
-        
-        // result =  type ? await openPackagePage(browser, page) : null
-
-        // return result
+        return repoPackage
     })
 
-
-    // console.log({packages});
-    await browser.close();
+    browser.close();
+    // const packageVersion = await latestVersion('@testing-library/jest-dom')
 
     res.send({packages})
 });
